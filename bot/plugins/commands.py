71513905a1369aaaeeffe2dc9eb295b0c2d5af6d
Cinemavillaz <87010768+Cinemavillaz@users.noreply.github.com>
2021-07-30 09:40:39 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -12,7 +12,7 @@ db = Database()
 
 @Client.on_message(filters.command(["start"]) & filters.private, group=1)
 async def start(bot, update):
-    update_channel = "@ch_hindi"
+    update_channel = "@cv_updatez"
     if update_channel:
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
@@ -22,9 +22,9 @@ async def start(bot, update):
         except UserNotParticipant:
             #await update.reply_text(f"Join @{update_channel} To Use Me")
             await update.reply_text(
-                text="𝐘𝐨𝐮 𝐦𝐮𝐬𝐭 𝐣𝐨𝐢𝐧 𝐨𝐮𝐫 𝐜𝐡𝐚𝐧𝐧𝐞𝐥 𝐨𝐭𝐡𝐞𝐫𝐰𝐢𝐬𝐞 </b>",
+                text="🔊 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 🤭  /n/nനിങ്ങൾക് സിനിമകൾ വെന്നോ? അതിനായി അത്യം ങ്ങളുടെ മെയിൻ ചാനലിൽ ജോയിൻ ചെയ്യണം... 😁  /n/nJoin ചെയതത്തിനു ശേഷം വീണ്ടും ബോട്ട് /start ആക്കൂ.😁 </b>",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text=" 🔰JOIN OUR CHANNEL🔰 ", url=f"https://t.me/ch_hindi")]
+                    [ InlineKeyboardButton(text=" 🔰JOIN OUR CHANNEL🔰 ", url=f"https://t.me/cv_updatez")]
               ])
             )
             return
@@ -54,7 +54,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '⭕️New Releases⭕️', url="https://t.me/new_movie_z"
                                 )
                         ]
                     ]
@@ -66,12 +66,12 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Group', url='https://t.me/cinehut'),
-        InlineKeyboardButton('channel', url ='https://t.me/ch_hindi')
+        InlineKeyboardButton('⚠️ 𝙂𝙍𝙊𝙐𝙋', url='https://t.me/cv_group1'),
+        InlineKeyboardButton('👷 𝘾𝙍𝙀𝘼𝙏𝙊𝙍', url ='https://t.me/cv_groupAdmin2')
     ],[
-        InlineKeyboardButton('source code', url='https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('📺 𝙅𝙊𝙄𝙉 𝙊𝙐𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇 📺', url='https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot-V2')
     ],[
-        InlineKeyboardButton('Help ⚙', callback_data="help")
+        InlineKeyboardButton('💡 𝙃𝙀𝙇𝙋', callback_data="help")
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
